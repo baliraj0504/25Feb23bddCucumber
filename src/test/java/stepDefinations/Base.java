cb3d7ab6ea9cad26137c8b111fd31bba6c60d36b
baliraj0504 <baliraj0504@gmail.com>
2023-05-21 10:05:51 +0530

    Updated 25 feb batch code

---
+++ src/test/java/stepDefinations/Base.java
@@ -0,0 +1,16 @@
+package stepDefinations;
+
+import org.openqa.selenium.WebDriver;
+
+import pageObject.AddNewCustomerPage;
+import pageObject.AdminPage;
+import pageObject.Vendor;
+
+public class Base {
+
+	public WebDriver driver;
+	public AdminPage ad;
+	public AddNewCustomerPage addCust;
+	
+	public Vendor vendor;
+}
